fdbffc876052df31799c61e08603d7d7306073de
Pranav Teja Aluvala <pranavteja.aluvala@gmail.com>
2026-07-03 19:13:55 +0530

    Add simple task manager functionality
    Implemented a simple task manager with options to add and view tasks. Includes error handling for user input and file operations.

--- file-handling/files.py
+++ file-handling/files.py
@@ -1,1 +1,87 @@
+import os
 
+# File Setup
+
+DATA_FOLDER = "data"
+TASK_FILE = os.path.join(DATA_FOLDER, "tasks.txt")
+
+os.makedirs(DATA_FOLDER, exist_ok=True)
+
+
+
+def add_tasks():
+    # Add tasks to the text file.
+
+    try:
+        total = int(input("How many tasks do you want to add? : "))
+
+        with open(TASK_FILE, "a") as file:
+            for i in range(total):
+                task = input(f"Task {i + 1}: ").strip()
+
+                if task:
+                    file.write(task + "\n")
+
+        print("\n Tasks added successfully!")
+
+    except ValueError:
+        print(" Please enter a valid number.")
+
+
+def view_tasks():
+    # Display all saved tasks.
+
+    try:
+        with open(TASK_FILE, "r") as file:
+            tasks = file.readlines()
+
+        if not tasks:
+            print("\n No tasks found.")
+            return
+
+        print("\n========== YOUR TASKS ==========")
+
+        for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task.strip()}")
+
+        print("===============================\n")
+
+    except FileNotFoundError:
+        print("Task file not found.")
+
+
+def menu():
+
+    while True:
+
+        print("\n====== SIMPLE TASK MANAGER ======")
+        print("1. Add Tasks")
+        print("2. View Tasks")
+        print("3. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            add_tasks()
+
+        elif choice == "2":
+            view_tasks()
+
+        elif choice == "3":
+            print("\n Session Ended.")
+            break
+
+        else:
+            print("Invalid option.")
+
+
+
+if __name__ == "__main__":
+    try:
+        menu()
+
+    except KeyboardInterrupt:
+        print("\nProgram Interrupted.")
+
+    except Exception as e:
+        print("Unexpected Error:", e)
